2af1aee60ef3cb5cffd4b976924f252c8f38ef25
Thanus Kumaar <thanuskumaara@gmail.com>
2024-05-01 22:42:52 +0530

    File for internal logic is created

---
+++ iot-management-system/src/app/hybridDataStructure.js
@@ -0,0 +1,13 @@
+class iotDevice {
+  constructor(name, type, ) {
+      this.data = data;
+      this.children = [];
+  }
+}
+
+class iotNetwork {
+  constructor(rootData) {
+      this.root = new iotDevice(rootData);
+  }
+}
+
