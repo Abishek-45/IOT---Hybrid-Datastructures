764d67cd33027281bba7578760075c81129cacec
Thanus Kumaar <thanuskumaara@gmail.com>
2024-05-01 21:47:20 +0530

    Basic directory structure and environment setup complete

---
+++ iot-management-system/src/app/layout.js
@@ -0,0 +1,17 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "IoT device management system",
+  description: "An application for controlling and managing IoT devices. Uses a single data structure for the functioning.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
