d5c72278ca57573b3a272c25eb78988c88286fb6
Aakash B <aakash735cse@gmail.com>
2024-05-09 17:27:31 +0530

    Home page completed(except font family)

--- iot-management-system/src/app/components/teamCard.js
+++ iot-management-system/src/app/components/teamCard.js
@@ -4,7 +4,7 @@ import { BiLogoGmail } from "react-icons/bi";
 
 export default function teamCard() {
   return (
-    <div class="absolute p-5 text-center flex flex-col justify-end left-1/4 top-1/3 h-[200px] w-[300px] rounded-xl bg-[#062F80] shadow-2xl">
+    <div class="relative p-5 text-center flex flex-col justify-end left-1/4 top-1/3 h-[200px] w-[300px] rounded-xl bg-[#062F80] shadow-2xl">
       <div class="absolute origin-center -top-1/4 left-1/3 rounded-full bg-white border-black border-2 h-[110px] w-[110px]"></div>
       <div class=" text-[28px] mb-4">Thanus Kumaar A</div>
       <div class=" flex flex-row gap-8 mt-2 mx-auto">
