a8c6033feb4c35f8bb4a5dba8384cb8fe4dcf163
Abishek-45 <abishek13105@gmail.com>
2024-05-10 01:12:03 +0530

    modifiing the navbar

--- iot-management-system/src/app/components/teamCard.js
+++ iot-management-system/src/app/components/teamCard.js
@@ -3,11 +3,10 @@ import { FaLinkedin } from "react-icons/fa";
 import { BiLogoGmail } from "react-icons/bi";
 import Image from "next/image";
 
-export default function teamCard({name, links, photosrc}) {
+export default function teamCard({ name, links, photosrc }) {
   return (
     <div class="relative p-5 text-center flex flex-col justify-end m-5 h-[200px] w-[300px] rounded-xl bg-[#062F80] shadow-2xl hover:scale-105 transition duration-150">
-      <div class="absolute origin-center -top-1/4 left-1/3 rounded-full bg-white border-black border-[3px] h-[110px] w-[110px]"
-      >
+      <div class="absolute origin-center -top-1/4 left-1/3 rounded-full bg-white border-black border-[3px] h-[110px] w-[110px]">
         <Image className="rounded-full p-1 scale-110" src={photosrc} fill />
       </div>
       <div class=" text-[28px] mb-4">{name}</div>
