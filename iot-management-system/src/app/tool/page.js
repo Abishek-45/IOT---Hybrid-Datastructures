ce0da1f10de194d4af6493df7d3f7a38244b6009
Thanus Kumaar <thanuskumaara@gmail.com>
2024-05-10 00:38:04 +0530

    Added drawer for navbar to navigate between pages

---
+++ iot-management-system/src/app/tool/page.js
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar"
+
+export default function Tool(){
+  return(
+    <main className="min-h-screen m-0 p-1 bg-[#CADCFC] ">
+      <Navbar />
+    </main>
+  )
+}
