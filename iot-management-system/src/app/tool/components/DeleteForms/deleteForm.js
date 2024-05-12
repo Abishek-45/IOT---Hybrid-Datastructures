8cd84b6007dd82bed95a14c6d158c1bfe773c698
Aakash B <aakash735cse@gmail.com>
2024-05-13 01:46:19 +0530

    Updated Styling in the Delete forms

--- iot-management-system/src/app/tool/components/DeleteForms/deleteForm.js
+++ iot-management-system/src/app/tool/components/DeleteForms/deleteForm.js
@@ -58,8 +58,8 @@ export default function DeleteForm({
                 )}
             </div>
             <div className="flex flex-row justify-end gap-4 mt-5">
-              <button onClick={handleFormSubmit}>Submit</button>
-              <button onClick={handleCloseFunction}>Cancel</button>
+                  <button onClick={handleFormSubmit} className="bg-gradient-to-r px-3 from-[#08134e] to-[#03379e] transition duration-300 hover:scale-105 active:opacity-60 text-white h-[30px] w-[80px] rounded-2xl">Submit</button>
+                  <button onClick={handleCloseFunction} className="bg-[#9FBEF7] h-[30px] w-[80px] border-2 border-[#03379e] transition duration-300 hover:scale-105 active:opacity-60 text-[#03379e] rounded-2xl">Cancel</button>
             </div>
           </DialogContentText>
         </DialogContent>
